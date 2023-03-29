7cad73b5c7b33b39e1f5da0288ab70203955b705
Julia <juliamarques61@gmail.com>
2023-03-29 19:17:24 -0300

    feat: add other files

---
+++ app/src/main/java/com/ada/pokedex_android_kotlin/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ada.pokedex_android_kotlin.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ada.pokedex_android_kotlin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
